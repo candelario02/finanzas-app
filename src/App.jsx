8d0e5ee33b425a88ba110b4a889f331de654b010
candelario02 <huamancarrioncande22@gmail.com>
2025-12-29 21:18:40 -0500

    cambio de letra diversion

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,7 @@ function App() {
 
         <div className="input-section">
           <div className="quick-tags">
-            {["GNV", "Comida", "Diversion", "Generé"].map(cat => (
+            {["GNV", "Comida", "Diversión", "Generé"].map(cat => (
               <button key={cat} onClick={() => setNombre(cat)} className="tag-btn">{cat}</button>
             ))}
           </div>
